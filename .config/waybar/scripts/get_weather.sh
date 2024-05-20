7e71f213fda499c23ff3f280afccb25ef6311bb0
kx1999 <kx1999@gmail.com>
2024-05-19 22:32:32 -0400

    =initial

---
+++ .config/waybar/scripts/get_weather.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+for i in {1..5}
+do
+    text=$(curl -s "https://wttr.in/?format=1&m")
+    if [[ $? == 0 ]]
+    then
+        text=$(echo "$text" | sed -E "s/\s+/ /g")
+        tooltip=$(curl -s "https://wttr.in/?format=%l:+%c+%t+(%f)+%w&m")
+        if [[ $? == 0 ]]
+        then
+            tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
+            echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
+            exit
+        fi
+    fi
+    sleep 2
+done
+echo "{\"text\":\"error\", \"tooltip\":\"error\"}"
