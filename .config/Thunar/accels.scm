ec9debb1750b12925f2e20aaf248723ddc6a493e
kx1999 <kx1999@gmail.com>
2024-08-27 11:54:48 -0400

    changes and remove thunar/accels

--- .config/Thunar/accels.scm
+++
@@ -1,9 +0,0 @@
-; Thunar GtkAccelMap rc-file         -*- scheme -*-
-; this file is an automated accelerator map dump
-;
-(gtk_accel_path "<Actions>/ThunarStandardView/back-alt" "<Alt>Left")
-(gtk_accel_path "<Actions>/ThunarActionManager/move-to-trash" "Delete")
-(gtk_accel_path "<Actions>/ThunarStandardView/back" "Back")
-(gtk_accel_path "<Actions>/ThunarWindow/switch-previous-tab" "<Primary><Shift>ISO_Left_Tab")
-(gtk_accel_path "<Actions>/ThunarWindow/switch-next-tab" "<Primary>Tab")
-(gtk_accel_path "<Actions>/ThunarStandardView/forward" "Forward")
